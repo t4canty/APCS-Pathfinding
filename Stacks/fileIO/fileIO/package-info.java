ecb1d3390288d9d088b88209958ef05e1fa0769b
Tom <t4canty@gmail.com>
2020-02-28 09:33:28 -0800

    Added package info.

---
+++ Stacks/fileIO/fileIO/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains classes for reading and parsing map files.
+ * FileReader - generic file reader that reads files into a DoublyLinkedList.
+ * MapParser - parses coordinate objects into a DLL from a file
+ * MapPoint - a custom object encapsulating the row, column, and data in a file. 
+ * @author Screencap
+ * https://github.com/t4canty
+ *
+ */
+package fileIO;
---
+++ Stacks/src/dataStructures/package-info.java
@@ -0,0 +1,11 @@
+
+/**
+ * Package for the following data structures:
+ * Stacks (both FiFo and LiFo)
+ * Singly-linked list
+ * Doubly-linked list
+ * @author Screencap
+ * https://github.com/t4canty
+ *
+ */
+package dataStructures;
